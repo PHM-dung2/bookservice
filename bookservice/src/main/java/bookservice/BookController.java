5f1bd2280a5ebbfd13d61bcabc6044ad205dae71
PHM-dung2 <pp4992@naver.com>
2025-02-10 11:55:48 +0900

    bookservice CURD 구현

--- bookservice/src/main/java/bookservice/BookController.java
+++ bookservice/src/main/java/bookservice/BookController.java
@@ -1,6 +1,9 @@
 package bookservice;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -42,14 +45,32 @@ public class BookController extends HttpServlet{
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(">> BOOK PUT RUN");
+		ObjectMapper mapper = new ObjectMapper();
+//		bno 가져오기
+		int bno = Integer.parseInt( req.getParameter("bno") );
+//		현재 시간 가져오기
+		LocalDateTime now = LocalDateTime.now();
+//		yyyy/mm/dd hh:mm:ss 형식으로 가져오기
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss");
+		String bupdate = now.format(formatter);
+		System.out.println(bupdate);
 		
+		BookDto bookDto = mapper.readValue( req.getReader() , BookDto.class );
+		bookDto.setBno(bno);
+		bookDto.setBupdate(bupdate);
+		boolean result = BookDao.getInstance().update(bookDto);
+		resp.setContentType("application/json");
+		resp.getWriter().print(result);
 	} // f end
 	
 //	4. 도서 정보 삭제
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(">> BOOK DELETE RUN");
-		
+		int bno = Integer.parseInt( req.getParameter("bno") );
+		boolean result = BookDao.getInstance().delete(bno);
+		resp.setContentType("application/json");
+		resp.getWriter().print(result);
 	} // f end
 	
 	
--- bookservice/src/main/java/bookservice/BookDao.java
+++ bookservice/src/main/java/bookservice/BookDao.java
@@ -1,8 +1,10 @@
 package bookservice;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import lombok.Getter;
@@ -31,33 +33,58 @@ public class BookDao extends Dao{
 	
 //	2. 도서 정체 정보 조회
 	public ArrayList<BookDto> findAll() {
-		ArrayList<BookDto> result = new ArrayList();
+		ArrayList<BookDto> result = new ArrayList<>();
 		try {
 			String sql = "select * from book";
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
 			while( rs.next() ) {
-				
-			}
+				BookDto bookDto = new BookDto();
+				bookDto.setBno(rs.getInt("bno"));
+				bookDto.setBname(rs.getString("bname"));
+				bookDto.setBwriter(rs.getString("bwriter"));
+				bookDto.setBpublisher(rs.getString("bpublisher"));
+				bookDto.setBdate(rs.getString("bdate"));
+				bookDto.setBupdate(rs.getString("bupdate"));
+				result.add(bookDto);
+			} // w end
 		}catch( SQLException e ) { System.out.println(e); }
 		return result;
 	} // f end
 	
 //	3. 도서 상세 정보 조회
-	public boolean findByBno() {
-		
-		return false;
-	} // f end
+//	public BookDto findByBno( int bno ) {
+//		
+//		return result;
+//	} // f end
 	
 //	4. 도서 정보 수정
-	public boolean update() {
-		
+	public boolean update( BookDto bookDto ) {
+		try {
+			String sql = "update book "
+					+ "set bname = ?, bwriter = ?, bpublisher = ?, bupdate = ? "
+					+ "where bno = ? ";
+			PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setString(1, bookDto.getBname());
+				ps.setString(2, bookDto.getBwriter());
+				ps.setString(3, bookDto.getBpublisher());
+				ps.setString(4, bookDto.getBupdate());
+				ps.setInt(5, bookDto.getBno());
+			int count = ps.executeUpdate();
+			if( count == 1 ) { return true; }
+		}catch( SQLException e ) { System.out.println(e); }
 		return false;
 	} // f end
 	
 //	5. 도서 정보 삭제
-	public boolean delete() {
-		
+	public boolean delete( int bno ) {
+		try {
+			String sql = "delete from book where bno = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setInt(1, bno);
+			int count = ps.executeUpdate();
+			if( count == 1 ) { return true; }
+		}catch( SQLException e ) { System.out.println(e); }
 		return false;
 	} // f end
 	
